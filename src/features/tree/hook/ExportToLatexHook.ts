89e77c3a8e09ebd54e81ab1da40b2cdeb13fc0f4
Vladyslav Futrak <vladyslav.futrak@student.tuke.sk>
2025-02-15 16:16:05 +0100

    fix(tex generation): show aliases int tex

--- src/features/tree/hook/ExportToLatexHook.ts
+++ src/features/tree/hook/ExportToLatexHook.ts
@@ -3,20 +3,21 @@ import {preprocessString} from "../../../core/utils";
 
 export function useExportToLatex() {
 
-  function exportToLatex(tree: ProofNode): string {
+  function exportToLatex(tree: ProofNode, showAliases: boolean): string {
     let latex = '\\begin{prooftree} \n';
 
     const traverseTree = (node: ProofNode, level: number = 0): string => {
       let nodeLatex = '';
 
       const premisesCount = node.premises ? node.premises.length : 0;
+      const nodeConclusion = showAliases ? node.wrappedConclusionWithAlias : node.wrappedConclusion;
 
       if (node.premises) {
         for (let premise of node.premises)
           nodeLatex += traverseTree(premise, level + 1);
-        nodeLatex += `  \\infer${premisesCount}[${node.rule}]{${node.wrappedConclusion} }\n`;
+        nodeLatex += `\t\\infer${premisesCount}[${node.rule}]{${nodeConclusion} }\n`;
       } else
-        nodeLatex += `  \\hypo{${node.wrappedConclusion}}\n`;
+        nodeLatex += `\t\\hypo{${nodeConclusion}}\n`;
 
       return nodeLatex;
     };
@@ -29,11 +30,12 @@ export function useExportToLatex() {
   }
 
   //todo : aliases show
-  const exportToBussproofs = (tree: ProofNode): string => {
+  const exportToBussproofs = (tree: ProofNode, showAliases: boolean): string => {
     let latex = '\\begin{prooftree} \n';
 
     const traverseTree = (node: ProofNode, level: number = 0): void => {
       const premisesCount = node.premises ? node.premises.length : 0;
+      const nodeConclusion = showAliases ? node.wrappedConclusionWithAlias : node.wrappedConclusion;
 
       if (node.premises) {
 
@@ -41,27 +43,27 @@ export function useExportToLatex() {
           traverseTree(premise);
         }
 
-        latex += '  \\RightLabel{$' + node.rule + '$}\n';
+        latex += '\t\\RightLabel{$' + node.rule + '$}\n';
         switch (premisesCount) {
           case 1:
-            latex += '  \\UnaryInfC{$' + node.wrappedConclusion + '$}\n';
+            latex += '\t\\UnaryInfC{$' + nodeConclusion + '$}\n';
             break;
           case 2:
-            latex += '  \\BinaryInfC{$' + node.wrappedConclusion + '$}\n';
+            latex += '\t\\BinaryInfC{$' + nodeConclusion + '$}\n';
             break;
           case 3:
-            latex += '  \\TrinaryInfC{$' + node.wrappedConclusion + '$}\n';
+            latex += '\t\\TrinaryInfC{$' + nodeConclusion + '$}\n';
             break;
           case 4:
-            latex += '  \\QuaternaryInfC{$' + node.wrappedConclusion + '$}\n';
+            latex += '\t\\QuaternaryInfC{$' + nodeConclusion + '$}\n';
             break;
           case 5:
-            latex += '  \\QuinaryInfC{$' + node.wrappedConclusion + '$}\n';
+            latex += '\t\\QuinaryInfC{$' + nodeConclusion + '$}\n';
             break;
         }
 
       } else {
-        latex += '  \\AxiomC{$' + node.wrappedConclusion + '$}\n';
+        latex += '\t\\AxiomC{$' + nodeConclusion + '$}\n';
       }
     }
 
