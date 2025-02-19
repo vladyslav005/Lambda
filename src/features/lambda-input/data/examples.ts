ae4cf0dfc8e8150eec865d01dcd2e5b6786d7efb
Vladyslav Futrak <vladyslav.futrak@student.tuke.sk>
2025-02-19 17:11:40 +0100

    feat(editor): add examples

--- src/features/lambda-input/data/examples.ts
+++ src/features/lambda-input/data/examples.ts
@@ -9,19 +9,34 @@ P = λ g: α -> α . (g x) : (α -> α) -> α;
 Q = λ h: α -> α . (h x) : (α -> α) -> α; 
 
 (N (M N (P (y))));`
-},
+  },
   {
     name: "Variants",
-    code: `PhysicalAddr = < firstlast : String, addr : String> ;
-VirtualAddr = <name : String, email : String>;
-Addr = [physical : PhysicalAddr, virtual : VirtualAddr];
+    code: `typedef PhysicalAddr = < firstlast : String, addr : String> ;
+typedef VirtualAddr = <name : String, email : String>;
+
+typedef Addr = [physical : PhysicalAddr, virtual : VirtualAddr]; // define variant type
 
 pa : PhysicalAddr;
-a = [physical = pa] as Addr;
+a = [physical = pa] as Addr; // make injection
 
-case a of
+case a of                       // use case of construction injection
      [physical = x] => x.firstlast
   || [virtual = y] => y.name;`
+  },
+  {
+    name: "Bianry variants",
+    code: `typedef PhysicalAddr = < firstlast : String, addr : String>;
+typedef VirtualAddr = <name : String, email : String>;
+
+typedef Addr = PhysicalAddr + VirtualAddr; // define variant type
+
+pa : PhysicalAddr;
+a = inl pa as Addr; // make injection
+
+case a of                // use case of construction injection
+    inl x => x.firstlast
+ || inr y => y.name;`
   },
   {
     name: "Tuples",
@@ -33,7 +48,48 @@ tuple = <var1, var2> : A * B;
 tuple; 
 tuple.1; 
 tuple.2;`
+  },
+  {
+    name: "Records",
+    code: `typedef Human = <name: String, age: Nat>; // define type
+
+john : Human; // define var of type 'Human'
+
+john.name; john.age; // use record projection`
+  },
+  {
+    name: "If condition",
+    code: `typedef Human = <name: String, fullName: String, age: Nat>; // define type
+
+john : Human; // define var of type 'Human
+
+showFullName = true : Bool;
+
+if showFullName then 
+    john.fullName
+else 
+    john.name
+;`
+  },
+  {
+    name: "Lists",
+    code: `list = nil[A] : List A; // create an empty list
+
+var : A;
+
+list;
+
+list = cons[A] var list; // add var to list
+
+isnil[A] list; // check if list is empty
+
+head[A] list; // get first element
+
+tail[A] list; // get list without first element
+
+`
   }
+
 
 
 ];
