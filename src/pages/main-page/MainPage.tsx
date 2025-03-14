bd5f785dde296cb8b69b63da34b4404ceef9f995
Vladyslav Futrak <vladyslav.futrak@student.tuke.sk>
2025-03-15 00:41:49 +0100

    feat: resizable layout

--- src/pages/main-page/MainPage.tsx
+++ src/pages/main-page/MainPage.tsx
@@ -1,80 +1,100 @@
 import {ErrorOutput} from "../../features/error-output/component/ErrorOutput";
-import React, {lazy, Suspense, useContext, useEffect, useState} from "react";
+import React, {lazy, Suspense, useEffect, useState} from "react";
 import {LoadingIndicator} from "../../common/components/loading/LoadingIndicator";
-import {IconButton} from "../../common/components/button/IconButton";
-import {MdOutlineKeyboardDoubleArrowRight} from "react-icons/md";
-import {AnimatePresence, motion} from "framer-motion";
-import {ConfigurationContext} from "../../features/configurations/context/ConfigurationContext";
 import {Header} from "../../common/header/Header";
+import {GammaContent} from "../../features/tree/component/gamma/GammaContent";
+import {Panel, PanelGroup, PanelResizeHandle} from "react-resizable-panels";
+import {Tab, TabList, TabPanel, Tabs} from 'react-aria-components';
 
 const TreeFlat = lazy(() => import('../../features/tree/component/TreeFlat'))
 const LambdaInput = lazy(() => import('../../features/lambda-input/component/LambdaInput'))
 
-export function MainPage() {
-  const [sidebarHidden, setSidebarHidden] = useState(false)
-  const [firstRender, setFirstRender] = useState(true);
+export enum Screen {
+  PC,
+  MOBILE
+}
 
+export function MainPage() {
+  const [showAliases, setShowAliases] = useState(false)
+
+  const [screen, setScreen] = useState<Screen>()
 
   useEffect(() => {
-    setFirstRender(false);
+    const observer = new ResizeObserver((entries) => {
+      for (let entry of entries) {
+        setScreen(entry.contentRect.width > 1024 ? Screen.PC : Screen.MOBILE);
+      }
+    });
+
+    observer.observe(document.body); // Observing the body for screen width changes
+
+    return () => observer.disconnect(); // Cleanup on unmount
   }, []);
 
   return (
       <div className="main-page flex flex-col relative">
         <Header/>
 
-        <div className="main-pane flex flex-col relative" style={{flexGrow: 10}}>
-          <div className="flex flex-col-reverse lg:flex-row grow relative"
+        {screen === Screen.PC &&
+            <PanelGroup direction={'vertical'} className="main-pane flex flex-col relative" style={{flexGrow: 10}}>
+                <Panel defaultSize={50} minSize={25} className="flex flex-col-reverse lg:flex-row grow relative">
+                    <PanelGroup direction={'horizontal'} className="flex  lg:flex-row grow relative"
+                                style={{marginRight: "1rem"}}>
+                        <Suspense
+                            fallback={
+                              <div className="lambda-input ui-block flex-row justify-center items-center">
+                                <LoadingIndicator/>
+                              </div>}
+                        >
+                            <Panel defaultSize={75} minSize={25} className="flex grow">
+                                <LambdaInput></LambdaInput>
+                            </Panel>
+                            <PanelResizeHandle className="resize resize-horizontal"/>
+                            <Panel className={`flex grow`} collapsible={true} defaultSize={25} minSize={20}
+                            >
+                                <ErrorOutput></ErrorOutput>
+                            </Panel>
+                        </Suspense>
+                    </PanelGroup>
+                </Panel>
 
-          >
-            <IconButton className={`hide-sidebar-btn ${sidebarHidden ? "rotated" : " "}`} style={{
-              margin: 0,
-              position: "absolute",
-              top: 30,
-              padding: 0,
-              right: 30,
-              zIndex: 10000,
-            }} onClick={() => setSidebarHidden(!sidebarHidden)} title={"Show/Hide errors"}>
-              <MdOutlineKeyboardDoubleArrowRight
-                  color="var(--M3-sys-light-on-secondary-container, var(--Schemes-On-Secondary-Container, #4A4459))"
-                  size={24}/>
-
-            </IconButton>
-            <Suspense
-                fallback={
-                  <div className="lambda-input ui-block flex-row justify-center items-center">
-                    <LoadingIndicator/>
-                  </div>}
-            >
-              <LambdaInput></LambdaInput>
-            </Suspense>
-            <AnimatePresence mode="wait">
-              {!sidebarHidden &&
-                  <motion.div className="flex flex-col grow lg:max-w-96"
-                              initial={firstRender ? false : {x: 400}}
-                              animate={{x: [400, 0]}}
-                              transition={{
-                                duration: 0.4,
-                                ease: "easeInOut",
-                                times: [0, .5, .7, .8, 1]
-                              }}
-                              exit={{x: 400}}
-
-                  >
-                      <ErrorOutput></ErrorOutput>
-                  </motion.div>
-              }
-            </AnimatePresence>
-          </div>
-          <div className="flex grow">
-            <Suspense
-                fallback={<div className="tree-flat-container ui-block"><LoadingIndicator/></div>}>
-              <TreeFlat></TreeFlat>
-            </Suspense>
-          </div>
-
-        </div>
-
+                <PanelResizeHandle className="resize resize-vertical"/>
+                <Panel defaultSize={50} minSize={25} className="flex grow">
+                    <Suspense
+                        fallback={<div className="tree-flat-container ui-block"><LoadingIndicator/></div>}>
+                        <PanelGroup direction={"horizontal"} className="flex grow relative"
+                                    style={{marginRight: "1rem"}}>
+                            <Panel minSize={25} defaultSize={75} className="flex grow">
+                                <TreeFlat showAliases={showAliases} setShowAliases={setShowAliases}/>
+                            </Panel>
+                            <PanelResizeHandle className="resize resize-horizontal"/>
+                            <Panel minSize={20} collapsible={true} defaultSize={25} className="flex grow">
+                                <GammaContent showAliases={showAliases}/>
+                            </Panel>
+                        </PanelGroup>
+                    </Suspense>
+                </Panel>
+            </PanelGroup>}
+        {screen === Screen.MOBILE &&
+            <div className="main-pane-mobile">
+              <Tabs>
+                  <TabList aria-label="History of Ancient Rome">
+                      <Tab id="FoR">Founding of Rome</Tab>
+                      <Tab id="MaR">Monarchy and Republic</Tab>
+                      <Tab id="Emp">Empire</Tab>
+                  </TabList>
+                  <TabPanel id="FoR">
+                      Arma virumque cano, Troiae qui primus ab oris.
+                  </TabPanel>
+                  <TabPanel id="MaR">
+                      Senatus Populusque Romanus.
+                  </TabPanel>
+                  <TabPanel id="Emp">
+                      Alea jacta est.
+                  </TabPanel>
+              </Tabs>
+            </div>
+        }
 
       </div>
   )
